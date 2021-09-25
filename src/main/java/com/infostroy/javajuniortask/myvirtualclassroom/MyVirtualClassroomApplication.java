38761eddf276dfa1f1713019c76e51de1918050f
Semen Skibin <skibin991@gmail.com>
2021-09-25 14:31:23 +0300

    Initial Commit

---
+++ src/main/java/com/infostroy/javajuniortask/myvirtualclassroom/MyVirtualClassroomApplication.java
@@ -0,0 +1,13 @@
+package com.infostroy.javajuniortask.myvirtualclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVirtualClassroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyVirtualClassroomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infostroy/javajuniortask/myvirtualclassroom/MyVirtualClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infostroy.javajuniortask.myvirtualclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVirtualClassroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
